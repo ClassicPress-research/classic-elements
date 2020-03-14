d10d3d78ecc6dcbf41bebc695cd4b624130cbbab
Zulfikar Gani <48245964+zulfgani@users.noreply.github.com>
2020-03-14 19:04:26 +0000

    Removing WP v check
    Removing the strict WP version check as the plugin is being refactored strictly for ClassicPress

--- elementor.php
+++ elementor.php
@@ -48,8 +48,6 @@ add_action( 'plugins_loaded', 'elementor_load_plugin_textdomain' );
 
 if ( ! version_compare( PHP_VERSION, '5.4', '>=' ) ) {
 	add_action( 'admin_notices', 'elementor_fail_php_version' );
-} elseif ( ! version_compare( get_bloginfo( 'version' ), '4.7', '>=' ) ) {
-	add_action( 'admin_notices', 'elementor_fail_wp_version' );
 } else {
 	require ELEMENTOR_PATH . 'includes/plugin.php';
 }
@@ -57,7 +55,7 @@ if ( ! version_compare( PHP_VERSION, '5.4', '>=' ) ) {
 /**
  * Load Classic Elements textdomain.
  *
- * Load gettext translate for Elementor text domain.
+ * Load gettext translate for Classic Elements text domain.
  *
  * @since 1.0.0
  *
@@ -79,22 +77,6 @@ function elementor_load_plugin_textdomain() {
 function elementor_fail_php_version() {
 	/* translators: %s: PHP version */
 	$message = sprintf( esc_html__( 'Classic Elements requires PHP version %s+, plugin is currently NOT RUNNING.', 'elementor' ), '5.4' );
-	$html_message = sprintf( '<div class="error">%s</div>', wpautop( $message ) );
-	echo wp_kses_post( $html_message );
-}
-
-/**
- * Classic Elements admin notice for minimum WordPress version.
- *
- * Warning when the site doesn't have the minimum required WordPress version.
- *
- * @since 1.5.0
- *
- * @return void
- */
-function elementor_fail_wp_version() {
-	/* translators: %s: WordPress version */
-	$message = sprintf( esc_html__( 'Classic Elements requires WordPress version %s+. Because you are using an earlier version, the plugin is currently NOT RUNNING.', 'elementor' ), '4.7' );
 	$html_message = sprintf( '<div class="error">%s</div>', wpautop( $message ) );
 	echo wp_kses_post( $html_message );
 }
